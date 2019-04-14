ca6c0144f88af9a050ee0708ed97d2b3313fca26
Maycon Pacheco <mayconjrpacheco@gmail.com>
2019-04-14 00:00:43 -0300

    Change admin display

--- experiments/admin.py
+++ experiments/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from experiments.models import Experiment
 
-admin.site.register(Experiment)
+# admin.site.register(Experiment)
+@admin.register(Experiment)
+class ExperimentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'domain', 'status',)
