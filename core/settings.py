d8924842897b9d846ebbeff4fe793bfec1ab456e
Matheus <matheusneto23@gmail.com>
2019-06-15 15:24:30 -0300

    revert settings

--- core/settings.py
+++ core/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'nutec'),
         'USER': os.environ.get('DB_USER', 'nutecadmin'),
         'PASSWORD': os.environ.get('DB_PASS', 'nc@2018'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'HOST': os.environ.get('DB_HOST', '10.1.112.28'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
@@ -167,7 +167,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 USE_X_FORWARDED_HOST = True
-# FORCE_SCRIPT_NAME='/api'
+FORCE_SCRIPT_NAME='/api'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
